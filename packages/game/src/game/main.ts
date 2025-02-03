259238f264c5d4651aa08067e9070fdfb2da5496
Frasydi <Frasydi137@Gmail.com>
2025-02-04 00:26:05 +0800

    disable  debug mode

--- packages/game/src/game/main.ts
+++ packages/game/src/game/main.ts
@@ -22,7 +22,7 @@ const config: Types.Core.GameConfig = {
         default :"arcade",
         arcade : {
             
-            debug : true
+            debug : false
         }
     },
     plugins: {
